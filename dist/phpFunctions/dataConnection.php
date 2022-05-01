221dc2bab65fdc63c5d60aad34fded4d297b4b04
Vladislav <103674945+Amper-r@users.noreply.github.com>
2022-05-01 20:45:42 +0300

    Update dataConnection.php

--- dist/phpFunctions/dataConnection.php
+++ dist/phpFunctions/dataConnection.php
@@ -2,14 +2,14 @@
     //XML FTP:
     $ftp_server = '37.140.192.158';
     $ftp_user_name = 'u1656895_practic';
-    $ftp_user_pass = 'gP8mO4iH0aiU3g';
+    $ftp_user_pass = '';
 
     $ftp_data = array("ftp_server" => $ftp_server, "ftp_user_name" => $ftp_user_name, "ftp_user_pass" => $ftp_user_pass);
 
     //Bd:
     $server = "37.140.192.158";
     $username = "u1656895_practic";
-    $password = "gT4uP5fG3s";
+    $password = "";
     $dbname = "u1656895_practic";
     $charset = "utf8";
 ?>
